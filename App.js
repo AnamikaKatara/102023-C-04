ba84f65d9516dafc7083ae110777dc25be2cf8d0
AnamikaKatara <135863421+AnamikaKatara@users.noreply.github.com>
2023-10-18 19:27:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="jsonplaceholder.typicode.com/photos"/>
+        <p>
+         hello there
+        </p>
+        <a
+          className="App-link"
+          href="https://via.placeholder.com/600/92c952"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          click here for photos
+        </a>
+        <table>
+          <tr>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+        </table>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ image.js
@@ -0,0 +1,5 @@
+<html>
+    <div>
+        hello image
+    </div>
+</html>
